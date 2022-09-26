b180877778f102b43547565dab33bd7203b9307f
André Roberto <andre.robertolins2018@gmail.com>
2022-09-25 21:34:56 -0300

    25/09

---
+++ ControleDeContatos/Repositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using ControleDeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Repositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        UsuarioModel FindById(int id);
+        List<UsuarioModel> BuscarTodos();
+        UsuarioModel Adicionar(UsuarioModel contato);
+        UsuarioModel Atualizar(UsuarioModel contato);
+        bool Apagar(int id);
+    }
+}
